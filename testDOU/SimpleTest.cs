1735db573e13ff339f1784447dfb0443c720304e
Dmytro Vasyliuk <dvasyliuk@atlassian.com>
2017-10-19 20:39:26 +0300

    commit project creation

---
+++ testDOU/SimpleTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SimpleTest
+{
+    public class SimpleTest
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [Test]
+        public void OpenDou() {
+            
+            driver.Url = "https://dou.ua/";
+
+            Assert.True(driver.Title.Contains("DOU"), "Title is not correct");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
